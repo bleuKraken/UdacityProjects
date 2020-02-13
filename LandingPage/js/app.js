2ea11a5b30e0ca4a254b35d1598a82cc33cfcda4
erickkg <erick31641@yahoo.com>
2020-02-12 18:44:10 -0800

    Updating files. Changing "Main Idea" on site, adding lorem content

---
+++ LandingPage/js/app.js
@@ -0,0 +1,12 @@
+
+
+/*
+
+Refreshes the page every 30 seconds.
+- Commecnted out becasue it slows down page after a while.
+- don't leave on forever
+setTimeout(function(){
+   window.location.reload(1);
+}, 20000);
+
+*/
