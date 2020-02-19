c28b56d1535431e35cb21453b39ed0b7f76f10da
erickkg <erick31641@yahoo.com>
2020-02-18 23:33:06 -0800

    Changing class names. Cleaned sections. Added nav styling

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -1,8 +1,23 @@
 
 
+
+
+//Refresh page every 6 minutes
+setTimeout(function(){
+   window.location.reload(1);
+}, 360000);
+
+// ##################### Point of no return #######################
 /*
 
-Refreshes the page every 30 seconds.
+
+// get key down
+document.addEventListener("keydown", function(event) {
+  console.log(event.which);
+});
+
+
+# Refreshes the page every 30 seconds.
 - Commecnted out becasue it slows down page after a while.
 - don't leave on forever
 setTimeout(function(){
