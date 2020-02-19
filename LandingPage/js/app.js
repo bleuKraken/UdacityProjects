7904f47a12d9fe5262aff0cb5e5cd4c86272344d
erickkg <erick31641@yahoo.com>
2020-02-19 01:24:16 -0800

    Playing with js. Adding classes. Checking rubric

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -1,10 +1,49 @@
+/*
+Todo List
+- get event listener to print out which section I am hovering over
+- when hovering over a section, highlight that setion on the nav
+- create entire nav from javascript ? 
+
+*/
+
+// get first Nav item
+/*
+const navItem1 = document.getElementById('nav-item-1');
+*/
+// get nav container
+const navContainer = document.getElementById('nav');
+
+// create new nev item for navigation
+const newListItem = document.createElement('li');
+const newLink = document.createElement('a');
 
 
+// Add class to new link child
+newLink.classList.add('link');
+//add classes to newly created item
+newListItem.classList.add('nav-item');
+// add <a> tag to list item
+newListItem.appendChild(newLink);
+// Give new string name to newly created item
+newListItem.textContent = 'Product';
+
+// Append (add) newly created list item to the navigation container
+navContainer.appendChild(newListItem);
+
+
+
+
+// Adding Event Listeners
+/*
+navItem.addEventListener("click", function() {
+  document.getElementById('product-section').scrollIntoView();
+});
+*/
 
 
 //Refresh page every 6 minutes
-setTimeout(function(){
-   window.location.reload(1);
+setTimeout(function() {
+  window.location.reload(1);
 }, 360000);
 
 // ##################### Point of no return #######################
