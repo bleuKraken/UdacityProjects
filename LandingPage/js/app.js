051e25cee0d02014ce3de0b84f5fa3e24fcf7714
erickkg <erick31641@yahoo.com>
2020-02-20 10:53:29 -0800

    Added More JS and cleaned up comments. Changed classes names.

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -2,67 +2,54 @@
 Notes / TODO's
 - Love the process!
 - Smile :)
-*/
-console.log("Starting..");
 
+- try to refector or shrink javascript code
+*/
 
 // ######################## Build the nav links ###########################
-// get nav container
+// Global varaiables
 const navContainer = document.getElementById('nav');
-let pageSections = document.querySelectorAll('section'); // Number of sections
-let sectionTitles = document.querySelectorAll('.section-title'); //Grab title name of sections
+let pageSections = document.querySelectorAll('section');
+let sectionTitles = document.querySelectorAll('.section-title');
 
 // For every section title, create a nav item with the name of that section title
 for (let index = 0; index < pageSections.length; index++) {
+
+  // Set and id to every section on page. Example: section-0
+  pageSections[index].id = "section-" + index;
+
   // create new list item for navigation
   const newListItem = document.createElement('li');
   newListItem.classList.add('nav-item');
-  newListItem.id = "navItem" + index; // <li id="navItem0" class="nav-item"> </li>
-
-  // Create new link
+  newListItem.id = "nav-item-" + index;
   const newAnchor = document.createElement('a');
   newAnchor.classList.add('link');
-  newAnchor.classList.add('color-white'); // <a class="link color-white"> </a>
-  newAnchor.textContent = sectionTitles[index].innerHTML; // <a class="link color-white"> Product </a>
-  // Place <li> inside of <ul>
+  newAnchor.classList.add('color-white');
+  newAnchor.textContent = sectionTitles[index].innerHTML;
+  // Append the children
   navContainer.appendChild(newListItem);
-  // Place <a> inside of <li>
   newListItem.appendChild(newAnchor);
-}
+  // Result: <li id="navItem0" class="nav-item"> <a class="link color-white"> Product </a>  </li>
 
-// ######################## Find out which section is being hovered ###########################
-//Grab all of sections
-
-// For every section that there is...
-for (let sectionIndex = 0; sectionIndex < pageSections.length; sectionIndex++) {
-  // Add an event listener to each section..
-  pageSections[sectionIndex].addEventListener('mouseover', function() {
-    // Print out wich section is being hovered
-    console.log('Hovering section ' + sectionIndex);
+  // Add event listeners to page sections being hovered
+  pageSections[index].addEventListener('mouseover', function() {
     unselectAll();
-    document.getElementById("navItem" + sectionIndex).classList.add('active');
+    document.getElementById("nav-item-" + index).classList.add('active');
   });
-}
 
+  // Scroll to section area when a nav link gets clicked on
+  let navLink = document.getElementById('nav-item-' + index);
+  let sectionArea = document.querySelectorAll('section');
+  navLink.addEventListener('click', function() {
+    unselectAll();
+    sectionArea[index].scrollIntoView();
+  });
+};
 
-
-// ######################## Set a Nav item to active ###########################
-
+// ######################## Point of No Return, Functions Below ###########################
+// Unselect all link items from having a green background
 function unselectAll() {
   for (let navItems = 0; navItems < pageSections.length; navItems++) {
-    document.getElementById("navItem" + navItems).classList.remove('active');
-
+    document.getElementById("nav-item-" + navItems).classList.remove('active');
   }
 }
-
-
-
-
-
-
-//Refresh page every 6 minutes
-setTimeout(function() {
-  window.location.reload(1);
-}, 360000);
-
-// ##################### Point of no return #######################
