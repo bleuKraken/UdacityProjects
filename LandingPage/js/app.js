e9a034abe8041f9e4ab23d4ee57f1966729fd04e
erickkg <erick31641@yahoo.com>
2020-02-19 17:16:26 -0800

    Adding JS. Renaming some sections and classes

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -1,36 +1,70 @@
 /*
-Todo List
-- get event listener to print out which section I am hovering over
-- when hovering over a section, highlight that setion on the nav
-- create entire nav from javascript ? 
+Notes
 
+// TODO:
+- remove some of the comments when the code gets cemented in my head more! :D
 */
 
-// get first Nav item
-/*
-const navItem1 = document.getElementById('nav-item-1');
-*/
+// ######################## Build the nav ###########################
 // get nav container
 const navContainer = document.getElementById('nav');
+// Array of section titles
+let sectionTitles = document.querySelectorAll('.section-title'); //Grab title of sections
 
-// create new nev item for navigation
-const newListItem = document.createElement('li');
-const newLink = document.createElement('a');
+// For every section title, create a nav item with the name of that section title
+for (let index = 0; index < sectionTitles.length; index++) {
+  // create new list item for navigation
+  const newListItem = document.createElement('li');
+  newListItem.classList.add('nav-item'); // <li class="nav-item">
+  // Create new link
+  const newAnchor = document.createElement('a');
+  newAnchor.classList.add('link');
+  newAnchor.classList.add('color-white');                 // <a class="link color-white"> </a>
+  newAnchor.textContent = sectionTitles[index].innerHTML; // <a class="link color-white"> Product </a>
+  // Place <li> inside of <ul>
+  navContainer.appendChild(newListItem);
+  // Place <a> inside of <li>
+  newListItem.appendChild(newAnchor);
+}
+
+// ######################## Set Section to Active ###########################
+const section1 = document.querySelector('#product-section');
+
+section1.addEventListener("mouseover", function() {
+  console.log("Section 1");
+})
 
 
-// Add class to new link child
-newLink.classList.add('link');
-//add classes to newly created item
-newListItem.classList.add('nav-item');
-// add <a> tag to list item
-newListItem.appendChild(newLink);
-// Give new string name to newly created item
-newListItem.textContent = 'Product';
+/*
+for(let index = 0; index < sectionTitle.length; index++){
 
-// Append (add) newly created list item to the navigation container
-navContainer.appendChild(newListItem);
+  console.dir(sectionTitle[index].innerHTML);
+
+  //add classes to newly created item
+  newListItem.classList.add('nav-item');
+  // Add class to new link child
+  newAnchor.classList.add('link');
+  // add <a> tag to list item
+  newListItem.appendChild(newLink);
+  // Give new string name to newly created item
+  newListItem.textContent = sectionTitle[index].innerHTML;
+
+  // Append (add) newly created list item to the navigation container
+  navContainer.appendChild(newListItem);
+}
 
 
+console.log(newListItem);
+
+*/
+
+
+//Refresh page every 6 minutes
+setTimeout(function() {
+  window.location.reload(1);
+}, 360000);
+
+// ##################### Point of no return #######################
 
 
 // Adding Event Listeners
@@ -41,15 +75,12 @@ navItem.addEventListener("click", function() {
 */
 
 
-//Refresh page every 6 minutes
-setTimeout(function() {
-  window.location.reload(1);
-}, 360000);
-
-// ##################### Point of no return #######################
+// get first Nav item
 /*
+const navItem1 = document.getElementById('nav-item-1');
+*/
 
-
+/*
 // get key down
 document.addEventListener("keydown", function(event) {
   console.log(event.which);
