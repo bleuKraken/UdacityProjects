a78dfffb5a8a63ebfb275ea9814d1951ef14e88a
erickkg <erick31641@yahoo.com>
2020-02-26 00:08:17 -0800

    Trying out diffrent pictures. Cleaning code.

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -1,10 +1,3 @@
-/*
-Notes / TODO's
-- Love the process!
-- Smile :)
-
-*/
-
 // ######################## Build the nav links ###########################
 // Global varaiables
 const navContainer = document.getElementById('nav');
@@ -12,6 +5,15 @@ const navIcon = document.getElementById('nav-icon');
 let pageSections = document.querySelectorAll('section');
 let sectionTitles = document.querySelectorAll('.section-title');
 
+// When navbar is clicked, change to color success.
+navIcon.addEventListener('click', function() {
+  RemoveActiveBackground();
+  document.getElementById('nav-icon').classList.toggle('color-success');
+  //Make nav items visible or invisible
+  for (let index = 0; index < pageSections.length; index++) {
+    document.getElementById('nav-item-' + index).classList.toggle('visible');
+  }
+});
 
 // For every section title, create a nav item with the name of that section title
 for (let index = 0; index < pageSections.length; index++) {
@@ -19,32 +21,30 @@ for (let index = 0; index < pageSections.length; index++) {
   // Set and id to every section on page. Example: section-0
   pageSections[index].id = "section-" + index;
 
-  // create new list item for navigation
+  // Creating nav bar items with links to sections
   const newListItem = document.createElement('li');
+  const newAnchor = document.createElement('a');
   newListItem.classList.add('nav-item');
   newListItem.id = "nav-item-" + index;
-  const newAnchor = document.createElement('a');
   newAnchor.classList.add('link');
   newAnchor.classList.add('color-white');
   newAnchor.textContent = sectionTitles[index].innerHTML;
-  // Append the two variables created above
+  // Append the two constants above
   navContainer.appendChild(newListItem);
   newListItem.appendChild(newAnchor);
   // Result: <li id="navItem0" class="nav-item"> <a class="link color-white"> Product </a>  </li>
 
-  // Add event listeners to page sections being hovered
+  // Add Event isteners to page sections being hovered
   pageSections[index].addEventListener('mouseover', function() {
-    unselectAll();
+    RemoveActiveBackground();
     document.getElementById("nav-item-" + index).classList.add('active');
   });
-
-
 
   // Scroll to section area when a nav link gets clicked on
   let navLink = document.getElementById('nav-item-' + index);
   let sectionArea = document.querySelectorAll('section');
   navLink.addEventListener('click', function() {
-    unselectAll();
+    RemoveActiveBackground();
     sectionArea[index].scrollIntoView({
       behavior: 'smooth'
     });
@@ -54,59 +54,18 @@ for (let index = 0; index < pageSections.length; index++) {
   document.getElementById('nav-item-' + index).addEventListener('click', function() {
     document.getElementById('nav-item-' + index).classList.toggle('active');
   });
-};
 
-
-/*
-if(document.getElementById('nav-icon').classList.contains('visible')) {
-  console.log('menu is visible');
-  document.querySelectorAll('page').addEventListener('click', function() {
-    console.log('event listener added');
-    for (let index = 0; index < pageSections.length; index++) {
-
-      document.getElementById('nav-item-' + index).classList.toggle('visible');
-      document.getElementById('nav-icon').classList.toggle('color-success');
-      console.log('should be toggling');
-    }
-  });
-
-}
-*/
-
-
-// Add event listener to Mobile dropdown. (Only works on small width, does not work on actual mobile. yet)
-navIcon.addEventListener('click', function() {
-  unselectAll();
-
-  //Make nav items visible or invisible
-  for (let index = 0; index < pageSections.length; index++) {
-    document.getElementById('nav-item-' + index).classList.toggle('visible');
-  }
-
-  document.getElementById('nav-icon').classList.toggle('color-success');
-  console.log('mobile nav visible');
-
-
-
-});
-
-// Give the sections an event listener that will make the nav go away
-for (let index = 0; index < pageSections.length; index++) {
+  // Give the sections an event listener that will make the nav go away
   pageSections[index].addEventListener('click', function() {
     for (let index = 0; index < pageSections.length; index++) {
       document.getElementById('nav-item-' + index).classList.remove('visible');
     }
   });
-
 }
-
-
-
-
 
 // ######################## Point of No Return, Functions Below ###########################
 // Unselect all link items from having a green background
-function unselectAll() {
+function RemoveActiveBackground() {
   for (let navItems = 0; navItems < pageSections.length; navItems++) {
     document.getElementById("nav-item-" + navItems).classList.remove('active');
   }
