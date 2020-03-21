7be08e9510e7913cde760bc5f69d5b22071582a1
erickkg <erick31641@yahoo.com>
2020-03-20 20:24:04 -0700

    Cleaned html, styles, js. Added lots of setions, functions, classes.

--- WeatherJournal/server.js
+++ WeatherJournal/server.js
@@ -1,14 +1,12 @@
 // Empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = [];
 
 // Express will begin to run server and routes
 const express = require('express');
-
-//Start up an instance of the application
 const app = express();
 
 /* Dependencies below */
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 /* Middleware */
 // Config Express to use body-parser as middle-ware!
@@ -28,8 +26,7 @@ const port = 8000;
 //Spin up the server!
 const server = app.listen(port, listening);
 
-// const server = app.listen(port, ()=>{console.log('running on localhost: ${port}')})
-// Callback to debug
+// Callback
 function listening() {
   console.log('Server is alive!');
   console.log(`Running on localhost: ${port}`); // NOTE: USE `` and NOT ''
@@ -40,7 +37,7 @@ app.get('/all', sendData);
 function sendData (request, response) {
   response.send(projectData);
 };
-
+// POST route
 app.post('/add', callBack);
 function callBack (request, response) {
   projectData.push(request.body);
--- WeatherJournal/website/app.js
+++ WeatherJournal/website/app.js
@@ -1,80 +1,94 @@
-
-const generateButton = document.getElementById('generate-entry');
-
-
-generateButton.addEventListener('click', function() {
-  const textboxEntry = document.getElementById('journal-entry').value;
-
-});
-
-
-
+const apiKey = 'de6caf975ecf294524393c35547db499';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+// Get date with JS
+let currentDate = new Date();
+let newDate = currentDate.getMonth() + '/' + currentDate.getDate() + '/' + currentDate.getFullYear();
+document.getElementById('header-date').innerHTML = newDate;
+// Global Vars
+let journalPost = document.getElementById('journal-post').value;
+let zipValue = document.getElementById('zip').value;
 
 
+/* ################### Event listener on 'generate" button ################### */
+document.getElementById('submit-entry').addEventListener('click', performAction);
+//Function to run after clicking on "Generate" button
+function performAction(e) {
+  const journalPost = document.getElementById('journal-post').value;
+  const zipValue = document.getElementById('zip').value;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Got from Udacity
-const postData = async ( url = '', data = {})=>{
-    console.log(data);
-      const response = await fetch(url, {
-      method: 'POST',
-      credentials: 'same-origin',
-      headers: {
-          'Content-Type': 'application/json',
-      },
-     // Body data type must match "Content-Type" header
-      body: JSON.stringify(data),
-    });
-
-      try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-      }catch(error) {
-      console.log("error", error);
-      }
+  // Validation with if's
+  if (journalPost === '') {
+    alert("Please enter something in the textbox");
+    return console.log('Missing post.');
+  }
+  if (zipValue === "") {
+    alert("Please enter a Zipcode");
+    return console.log('Missing zipcode.');
   }
 
-postData('/add', {answer:42});
-
-/* ################# Another Example of app.js below
-
-//Function to POST data
-const postData = async ( url = '', data = {})=>{
-  console.log(data)
-    const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  });
-
-    try {
-      const newData = await response.json();
-      // console.log(newData);
-      return newData
-    }catch(error) {
-    console.log("error", error);
-    // appropriately handle the error
-    }
+  //Get API Data
+  getApiData(baseURL, zipValue, apiKey).then(function(fahrenheitTemp) {
+    postApiData('/add', {
+      temperature: fahrenheitTemp + "f",
+      date: newDate,
+      userResponse: journalPost,
+    });
+    // update UI
+    updateUI();
+  })
 }
 
-// TODO-Call Function
-postData('/addAnimal', {animal:'lion'})
+/* ################### Get API Data ################### */
+const getApiData = async (baseURL, zip, apiKey) => {
+  // Form URL request. Example: http://api.openweathermap.org/data/2.5/weather?zip=91790,us&appid=de6caf975ecf294524393c35547db499
+  const response = await fetch(baseURL + zip + ',us&appid=' + apiKey);
+  // Getting values from response
+  try {
+    const webData = await response.json();
+    // Get City name
+    let city = webData.name;
+    document.getElementById('city-name').innerHTML = city;
+    // Get temperature, then convert temp from Kelvin to Fahrenheit
+    let fahrenheitTemp = ((webData.main.temp - 273.15) * 9) / 5 + 32;
+    fahrenheitTemp = fahrenheitTemp.toFixed(2);
+    return fahrenheitTemp;
+  } catch (error) {
+    console.log("Error getting API data", error);
+  }
+}
 
-############################################################### End of second example */
+/* ################### Post API Data ################### */
+const postApiData = async (url = '', data = {}) => {
+
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  // Trying to get new data
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("Error posting API Data", error);
+  }
+};
+
+/* ################### Functions Below ################### */
+const updateUI = async () => {
+  // Attempting to input values from API
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json();
+    const mostRecentRecord = allData[allData.length - 1];
+    document.getElementById('date').innerHTML = mostRecentRecord.date;
+    document.getElementById('temp').innerHTML = mostRecentRecord.temperature;
+    document.getElementById('header-temp').innerHTML = mostRecentRecord.temperature;
+    document.getElementById('content').innerHTML = mostRecentRecord.userResponse;
+  } catch (error) {
+    console.log("Error updateing UI", error);
+  }
+}
