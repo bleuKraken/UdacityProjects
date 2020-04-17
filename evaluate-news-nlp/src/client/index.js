7510c62607f653dab4c1f470b0cecbaf46c0ddf5
erickkg <erick31641@yahoo.com>
2020-04-17 00:55:18 -0700

    News evaluation project, still working dev-mode out

---
+++ evaluate-news-nlp/src/client/index.js
@@ -0,0 +1,14 @@
+import { checkForName } from '../js/nameChecker';
+import { handleSubmit } from '../js/formHandler';
+
+import '../scss/resets.scss';
+import '../scss/base.scss';
+import '../scss/footer.scss';
+import '../scss/form.scss';
+import '../scss/header.scss';
+
+
+console.log("Var check for name is " + checkForName);
+
+alert("I EXIST");
+console.log("CHANGE!!");
