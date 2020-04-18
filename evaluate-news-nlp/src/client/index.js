a52324ad2dda6a82be1ef3d1566685ddb4311ef7
erickkg <erick31641@yahoo.com>
2020-04-18 13:13:04 -0700

    Changing styles, removed console log errors

--- evaluate-news-nlp/src/client/index.js
+++ evaluate-news-nlp/src/client/index.js
@@ -1,14 +1,13 @@
 import { checkForName } from '../js/nameChecker';
 import { handleSubmit } from '../js/formHandler';
 
-import '../scss/resets.scss';
-import '../scss/base.scss';
-import '../scss/footer.scss';
-import '../scss/form.scss';
-import '../scss/header.scss';
+import '../styles/resets.scss';
+import '../styles/base.scss';
+import '../styles/footer.scss';
+import '../styles/form.scss';
+import '../styles/header.scss';
 
 
 console.log("Var check for name is " + checkForName);
 
 alert("I EXIST");
-console.log("CHANGE!!");
