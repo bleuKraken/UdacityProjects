bdec46ae4f74bd305b83ef96a491feaaf137496c
erickkg <erick31641@yahoo.com>
2020-04-30 01:27:08 -0700

    Starting project travel-app. Running started

---
+++ travel-app/src/client/index.js
@@ -0,0 +1,15 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+
+console.log(checkForName);
+
+export {
+    checkForName,
+    handleSubmit
+}
