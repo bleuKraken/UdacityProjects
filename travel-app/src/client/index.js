cea4efb0e0490b6a7d935c35ddf6ad1efe77f92e
erickkg <erick31641@yahoo.com>
2020-06-02 23:58:53 -0700

    Small update. Added comments, working on JEST

--- travel-app/src/client/index.js
+++ travel-app/src/client/index.js
@@ -1,19 +1,16 @@
 import { application } from './js/application.js'
 import {StartCountdownTimer} from './js/countdownTimer.js'
-
-
+// Styles used
 import './styles/style.scss'
 
-
+// Event listeners used to populate dropdown and get API data
 document.getElementById('submit-country-button').addEventListener('click', () => {
   application(event);
 });
-
 
 document.getElementById('submit-area-button').addEventListener('click', () => {
   application(event);
 });
 
 
-alert("Hello from src client undex.js");
 export { application}
--- travel-app/src/client/js/application.test.js
+++
@@ -1,7 +0,0 @@
-const testApplication = require('./application.js');
-const testAPI = require('../../server/mockAPI.js');
-
-// TODO: ADD JEST TESTING
-test('Checks mockAPI', () => {
-  expect(testAPI).toBe("Greeting from the mockAPI");
-});
